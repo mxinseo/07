3168daeee0d2977982e28ec614d9ea7e1e91190b
rudalstj <jade201913@gmail.com>
2022-10-18 16:52:31 +0900

    04

--- main.c
+++ main.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void f(void);
 
+int i; 
 int main(int argc, char *argv[]) {
-	int i = 10;
-	
-	printf("함수 호출 전 i = %d\n", i); //처음 초기화 그대로 출력  
-	inc(i);
-	printf("함수 호출 후 i = %d\n", i); //함수 종료와 함께 매개변수 소멸   
-	
+	for(i=0; i<5; i++)
+	{
+		f();
+	}
 	return 0; 
 } 
-int inc(int counter)
+void f(void)
 {
-	counter++;
-	return counter;
+	for(i=0; i<10; i++)
+		printf("#"); 
 }
+
