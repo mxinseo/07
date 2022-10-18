63b1376a7f3877672c0a69e24540052c2da9a96d
rudalstj <jade201913@gmail.com>
2022-10-18 17:02:42 +0900

    05

--- main.c
+++ main.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void f(void);
+void sub(void);
 
-int i; 
 int main(int argc, char *argv[]) {
-	for(i=0; i<5; i++)
-	{
-		f();
-	}
+	int i;
+	for(i=0; i<3;i++)
+		sub();
 	return 0; 
 } 
-void f(void)
-{
-	for(i=0; i<10; i++)
-		printf("#"); 
-}
 
+void sub(void)
+{
+	int auto_count=0; //자동할당  
+	static int static_count = 0; //정적할당 
+	
+	//변수 1 증가  
+	auto_count++;
+	static_count++;
+	
+	//변수 내용 확인  
+	printf("auto_count=%d\n", auto_count);
+	printf("static_count=%d\n", static_count);
+}
