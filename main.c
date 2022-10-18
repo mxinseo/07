b8dc0c5d4c57a2ad158f60613b82f328d3c12043
rudalstj <jade201913@gmail.com>
2022-10-18 16:29:36 +0900

    02

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int flag = 1;
-	int y;
-	while(flag != 0)
+	int i;
+	
+	for(i=0;i<5;i++)
 	{
-		y = 3;
-		flag = 0;
-	}
-	y = 4;		//변수 y 정의가 while 블록에 있으면 오류  
+		int temp = 1;
+		printf("temp = %d\n", temp);
+		temp++;
+	 } 
+	 //temp에 대한 선언이 루프의 시작에 있어 temp = 1이 5번 출력  
 	return 0;
-}
+} 
